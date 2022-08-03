14037876aeda5a56b20ba6c356f3d8b895827196
Li, Chen Wei <99860942+weioncrypto@users.noreply.github.com>
2022-08-03 17:26:26 -0500

    Update 033. Search in Rotated Sorted Array.c

--- LeetCode/Medium/033. Search in Rotated Sorted Array.c
+++ LeetCode/Medium/033. Search in Rotated Sorted Array.c
@@ -14,3 +14,26 @@ int search(int* nums, int numsSize, int target){
     }
     return -1;
 }
+
+
+
+int search(int* nums, int numsSize, int target){
+    int left = 0, right = numsSize - 1;
+    while (left <= right)   
+    {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target) return mid;
+
+        if(nums[mid] < nums[right]) {
+            // 中間小於最右，右半邊有序，判斷新邊界
+            if(nums[mid] < target && nums[right] >= target) left = mid + 1;
+            else right = mid - 1;
+        }
+        else {
+            // 左半邊有序
+            if(nums[mid] > target && nums[left] <= target) right = mid - 1;
+            else left = mid + 1;
+        }
+    }
+    return -1;
+}
